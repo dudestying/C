eeb6ffa4e2da06bce12818c6882fb9d04e8da53f
Salma Mobasher <salmamob@gmail.com>
2017-06-08 23:23:06 -0400

    Create hero.c

---
+++ HeroGame/hero.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+#include <stdlib.h>
+#include"../include/hero.h"
+
+int main(int argc, char*argv[])
+{
+    if(validpar(argc)==FALSE)
+    {
+        return FALSE;
+    }
+    if(makeRooms(argv[1])==FALSE)
+    {
+        return FALSE;
+    }
+}
